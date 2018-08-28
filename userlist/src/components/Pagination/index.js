b50d1e3aea509ecb091ad70c645ade98813c6fff
BradHuang <adghbc01@tamu.edu>
2018-08-28 23:46:02 +0000

    to-do: if delete right after created a user, Asyn to be handled.

--- userlist/src/components/Pagination/index.js
+++ userlist/src/components/Pagination/index.js
@@ -32,19 +32,23 @@ const CurPage = (props) => {
 };
 
 class Pagination extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { numOfPages: 0, allPages: [] };
-    }
-    componentWillReceiveProps(props) {
+    // constructor(props) {
+        // super(props);
+        // this.state = { numOfPages: 0, allPages: [] };
+        // this.state = { numOfPages: 0 };
+    // }
+    // componentWillReceiveProps(props) {
         // console.log(props.data.numOfUsers, props.data.usersPerPage);
-        let nums = Math.ceil(props.data.numOfUsers / props.data.usersPerPage);
+        // let nums = Math.ceil(props.data.numOfUsers / props.data.usersPerPage);
         // console.log(nums);
+        // let pages = [];
+        // for (let p = 1; p <= nums; p++) { pages.push(p); }
+        // this.setState({ numOfPages: nums, allPages: pages });
+    // }
+    render() {
+        let nums = Math.ceil(this.props.data.numOfUsers / this.props.data.usersPerPage);
         let pages = [];
         for (let p = 1; p <= nums; p++) { pages.push(p); }
-        this.setState({ numOfPages: nums, allPages: pages });
-    }
-    render() {
         return (
             <nav aria-label="...">
                 <ul className="pagination">
@@ -52,7 +56,7 @@ class Pagination extends Component {
                     page={this.props.data.curPage}
                     changePage={this.props.changePage}
                 />
-                {this.state.allPages.map((p, index) => {
+                {pages.map((p, index) => {
                     return ((index + 1) === this.props.data.curPage) ?
                     (<CurPage 
                         key={index} pageNum={p} cur={true} changePage={this.props.changePage}
@@ -63,7 +67,8 @@ class Pagination extends Component {
                 })}
                 <GoNext 
                     page={this.props.data.curPage}
-                    lastPageNum={this.state.numOfPages}
+                    // lastPageNum={this.state.numOfPages}
+                    lastPageNum={nums}
                     changePage={this.props.changePage}
                 />
                 </ul>
